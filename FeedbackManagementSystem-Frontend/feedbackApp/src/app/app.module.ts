ba9b1a0f33edde4477294f5668372e9c8edadbfe
Tanmay0212 <52206583+Tanmay0212@users.noreply.github.com>
2020-05-13 12:44:28 +0530

    Update app.module.ts

--- FeedbackManagementSystem-Frontend/feedbackApp/src/app/app.module.ts
+++ FeedbackManagementSystem-Frontend/feedbackApp/src/app/app.module.ts
@@ -22,13 +22,9 @@ import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import { ParticipantComponent } from './participant/participant.component';
 import { ListFeedbackTrainingProgramComponent } from './list-feedback-training-program/list-feedback-training-program.component';
 import { ListParticipantComponent } from './list-participant/list-participant.component';
-<<<<<<< HEAD
 import { AddParticipantComponent } from './add-participant/add-participant.component';
-=======
-import { ErrorComponent } from './error/error.component';
 import { GlobalErrorService } from './service/global-error.service';
-
->>>>>>> 295b5338e008a75c6744056e52b9bfdca98cfb98
+import { ErrorComponent } from './error/error.component';
 
 const routes : Routes = [
   {path:'login', component:LoginComponent},
@@ -42,16 +38,10 @@ const routes : Routes = [
   {path:'add-feedback/:tId/:fId', component:AddFeedbackComponent},
   {path:'add-training',component:AddTrainingprogramComponent},
   {path:'list-training',component:ListTrainingProgramComponent},
-<<<<<<< HEAD
   {path:'list-participant', component:ListParticipantComponent},
   {path:'list-feedback-faculty/:id', component: ListFeedbackFacultyComponent},
   {path:'add-participant' , component:AddParticipantComponent},
-=======
-  {path:'participant', component:ParticipantComponent},
-  {path:'error/:errMsg', component:ErrorComponent},
-  {path:'list-feedback-faculty/:id', component: ListFeedbackFacultyComponent},
-  {path:'list-feedback-training-program/:tId', component: ListFeedbackTrainingProgramComponent},
->>>>>>> 295b5338e008a75c6744056e52b9bfdca98cfb98
+  {path:'error/:errMsg', component: ErrorComponent},
   {path:'', redirectTo:'home', pathMatch:"full"},
   {path:'*', redirectTo:'home', pathMatch:"full"}
 ];
@@ -73,14 +63,9 @@ const routes : Routes = [
     ListFeedbackFacultyComponent,
     AddEmployeeComponent,
     ParticipantComponent,
-<<<<<<< HEAD
     ListParticipantComponent,
-    AddParticipantComponent
-=======
-    ListFeedbackTrainingProgramComponent,
-    ListParticipantComponent,
+    AddParticipantComponent,
     ErrorComponent
->>>>>>> 295b5338e008a75c6744056e52b9bfdca98cfb98
   ],
   imports: [
     BrowserModule,
